49926951cae4cbced043b89d7dd33384c15ff817
Joseph Cheng <josephcheng88@gmail.com>
2016-02-23 23:17:39 +0000

    fixed alignment of pins through javascript edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
-//= require_tree .
 //= require masonry/jquery.masonry
 //= require masonry/jquery.imagesloaded.min
+//= require bootstrap
+//= require_tree .
+
