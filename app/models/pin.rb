ddff415fcd7af24c1d8d05c1a14619553687ab5a
Joseph Cheng <josephcheng88@gmail.com>
2016-02-24 00:34:55 +0000

    resized photo uploads

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,6 @@ class Pin < ActiveRecord::Base
      belongs_to :user
      validates :image, presence: true
      validates :description, presence: true
-     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+     has_attached_file :image, :styles => { :medium => "800x800>", :thumb => "300x300>" }
      validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
